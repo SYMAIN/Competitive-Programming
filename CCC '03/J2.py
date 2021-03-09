180a891ddbf2e8cc27b7ce3e90b3812dd2f7959d
QqWwEe123123123 <xxsimonxx119@gmail.com>
2021-03-09 14:11:02 -0500

    Update J2.py

--- CCC '03/J2.py
+++ CCC '03/J2.py
@@ -1,19 +1,13 @@
 import math
-
 while 1:
 	n = int(input())
-	if n == 0:
+	if n <= 0:
 		break
-	
-	l = 0
-	w = 0
-	perimeter = 1000000
 
-	for i in range(1,n):
-		p = (2 * (i**2) + n*2)/i
-		if p <= perimeter:
-			perimeter = int(p)
-			l = int(i)
+	for l in range(int(math.sqrt(n)),0,-1):
+		if n % l == 0:
 			w = n//l
-
-	print(f"Minimum perimeter is {perimeter} with dimensions {l} x {w}")
+			perimeter = l*2 + w*2
+			print(f"Minimum perimeter is {perimeter} with dimensions {l} x {w}")
+			break
+	
